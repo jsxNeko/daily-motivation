35d987f1551188e0161ca9fd4abbebaa7ba219f2
Serenuy <dserena01@yahoo.com>
2022-10-24 17:34:34 -0500

    Implement Introduction page/component

---
+++ src/components/Introduction.js
@@ -0,0 +1,23 @@
+import '../css/Introduction.css';
+import { useNavigate, Link } from 'react-router-dom';
+
+const Introduction = () => {
+	const navigate = useNavigate();
+	return(
+		<section className="introduction-section">
+			<div id="first-intro">Hello :)</div>
+			<div id="second-intro">
+				Good Morning<br/>
+				Buen Dia<br/>
+				Guten Morgen<br/>
+			</div>
+			<div id="third-intro">Let's get you motivated to continue your day.</div>
+			<div id="fourth-intro">
+				<p>Start by telling me a little about how you are feeling right now.</p>
+				<button className="btn btn-lg btn-continue-introduction rounded-5">Continue</button>
+			</div>
+		</section>
+	);
+};
+
+export default Introduction;
