390def16b7f5c48d8ad365132a76a1aeecfdb4da
Serenuy <dserena01@yahoo.com>
2022-10-25 15:45:15 -0500

    Load BackgroundColor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 import Introduction from './components/Introduction';
 import UserInformation from './components/UserInformation';
 import Home from './components/Home';
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Upon window loading, check if the user has selected a color previously
+window.onload = () => {
+  if(localStorage.getItem("background")) { 
+    document.body.style.backgroundColor = JSON.parse(localStorage.getItem("background"))
+  }
+};
 
 function App() {
   return (
@@ -16,6 +24,6 @@ function App() {
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
