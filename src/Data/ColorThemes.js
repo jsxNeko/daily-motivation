f964283123bdae936a34a5b6aa2036b777992417
Serenuy <dserena01@yahoo.com>
2022-10-25 15:45:29 -0500

    Create ColorTheme JSON

--- src/Data/ColorThemes.js
+++ src/Data/ColorThemes.js
@@ -1,7 +1,11 @@
-const colorTheme = {
-	"default": "#9eb4e7",
-	"black": "#000",
-	"orange": "orange",
+const ColorThemes = {
+	"background": [
+		{ "color": "#000" },
+		{ "color": "#b68675" },
+		{ "color": "#6c6c6c" },
+		{ "color": "#ffbfc4" },
+		{ "default": "#9eb4e7" },
+	],
 };
 
-export default colorTheme;
+export default ColorThemes;
