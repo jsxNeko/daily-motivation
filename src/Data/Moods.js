aa54b5f3f68c12662bb69a93c2e4cab46cf79013
Serenuy <dserena01@yahoo.com>
2022-10-25 15:43:39 -0500

    Create Mood JSON

---
+++ src/Data/Moods.js
@@ -0,0 +1,22 @@
+const Moods = {
+	"moods": [
+		{
+			"emotion": "Down",
+			"emotion": "Sadness",
+			"emotion": "Bored",
+			"emotion": "Lazy",
+			"emotion": "Fear",
+			"emotion": "Anger",
+			"emotion": "Happiness",
+			"emotion": "Shy",
+			"emotion": "Lonely",
+			"emotion": "Tired",
+			"emotion": "Optimistic",
+			"emotion": "Sleepy",
+			"emotion": "Skeptical",
+			"emotion": "Proud",
+		}
+	],
+};
+
+export default Moods;
