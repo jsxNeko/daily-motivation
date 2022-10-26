28a3589bb73854680c1920205e976f95a4f45ec8
Serenuy <dserena01@yahoo.com>
2022-10-26 17:58:19 -0500

    Implement react-select and moods data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,57 +1,93 @@
 import '../css/Home.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import Select from 'react-select';
 import ColorThemes from '../data/ColorThemes';
+import Moods from '../data/Moods';
 
 const Home = () => {
-	// Check if user has previously selected a color
-	// If not, select the default background color
-	const [ color, setColor ] = useState(localStorage.getItem("background") ? 
-										JSON.parse(localStorage.getItem("background")) :
-										'#9eb4e7');
+	const [ mood, setMood ] = useState("");
 
 	const changeTheme = (c) => {
-		setColor(c);
 		localStorage.setItem("background", JSON.stringify(c));
 		document.body.style.backgroundColor = JSON.parse(localStorage.getItem("background"));
 	};
 
+	const customStyles = {
+		control: (base,state) => ({
+			...base,
+			background: "rgba(0,0,0,0)",
+			borderColor: "#fff",
+			boxShadow: state.isFocused ? "0 0 2px 2px #fff" : null,
+			color: "#fff",
+			"&:hover": {
+				borderColor: "#fff",
+			},
+		}),
+		option: base => ({
+			...base,
+			color: "#1b1b1b"
+		}),
+		singleValue: base => ({
+			...base,
+			color: "#fff"
+		}),
+	};
+
+	const customThemes = (theme) => ({
+		...theme,
+		colors: {
+		    ...theme.colors,
+		    neutral20: '#fff',
+		    neutral50: '#fff',
+		    neutral80: '#fff',
+		    primary: "#ffcad8",
+		    primary25: "#ffb4c2",
+		    primary50: "#ffb4c2",
+		},
+	})
+
 	return(
 		<section id="homeSection">
 			<div className="home-grid">
-				<div className="home-item">
+				<div className="home-item1">
 					<p>Change color theme:</p>
 					{
 						// Loop through ColorThemes JSON and return color picker buttons
 						ColorThemes.background.map(c => {
 							if(c.color) {
 								return(
-									<button className="btn btn-light me-3 p-3 rounded-5" onClick={() => changeTheme(c.color)} style={{backgroundColor: c.color}}></button>
+									<button key={c.color} className="btn btn-light me-2 mb-3 p-3 rounded-5" onClick={() => changeTheme(c.color)} style={{backgroundColor: c.color}}></button>
 								);
 							} else {
 								return(
-									<><br/><button className="btn btn-light mt-3 rounded-5" onClick={() => changeTheme(c.default)}>Default</button></>
+									<><br/><button key={c.default} className="btn btn-light rounded-5" onClick={() => changeTheme(c.default)}>Default</button></>
 								);
 							}
 						})
 					}
 				</div>
-				<div className="home-item">
+				<div className="home-item2">
 					<div>
-						<label htmlFor="mood">Type what you are feeling right now:</label>
-						<input className="form-control mt-3" id="mood" type="text"/>
+						<label htmlFor="mood">How are you feeling right now:</label>
+						<Select
+						className="mt-2"
+						id="mood-input"
+						placeholder="Ex: sad, bored"
+						styles={customStyles}
+						theme={customThemes}
+						options={Moods}>
+						</Select>
 					</div>
+
 					<div className="mt-2">
 						<p>Suggestions:</p>
-						<button className="btn btn-transparent rounded-5 me-2">
+						<button className="btn btn-transparent rounded-5 me-1">
 							Down
 						</button>
-						<button className="btn btn-transparent rounded-5 me-2">
+						<button className="btn btn-transparent rounded-5 me-1">
 							Bored
 						</button>
-						<button className="btn btn-transparent rounded-5 me-2">
-							Anxious
-						</button>
-						<button className="btn btn-transparent rounded-5 me-2">
+						<button className="btn btn-transparent rounded-5 me-1">
 							Lazy
 						</button>
 					</div>
