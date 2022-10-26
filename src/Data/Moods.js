11701eec92b7cb0d049cd1eb95edc70207a9a9ad
Serenuy <dserena01@yahoo.com>
2022-10-26 17:57:01 -0500

    Update Moods JSON

--- src/Data/Moods.js
+++ src/Data/Moods.js
@@ -1,22 +1,14 @@
-const Moods = {
-	"moods": [
-		{
-			"emotion": "Down",
-			"emotion": "Sadness",
-			"emotion": "Bored",
-			"emotion": "Lazy",
-			"emotion": "Fear",
-			"emotion": "Anger",
-			"emotion": "Happiness",
-			"emotion": "Shy",
-			"emotion": "Lonely",
-			"emotion": "Tired",
-			"emotion": "Optimistic",
-			"emotion": "Sleepy",
-			"emotion": "Skeptical",
-			"emotion": "Proud",
-		}
-	],
-};
+const Moods = [
+	{ value: "down", label: "Down" },
+	{ value: "sadness", label: "Sadness" },
+	{ value: "bored", label: "Bored" },
+	{ value: "lazy", label: "Lazy" },
+	{ value: "anger", label: "Anger" },
+	{ value: "happiness", label: "Happiness" },
+	{ value: "shy", label: "Shy" },
+	{ value: "tired", label: "Tired" },
+	{ value: "skeptical", label: "Skeptical" },
+	{ value: "proud", label: "Proud" },
+];
 
 export default Moods;
