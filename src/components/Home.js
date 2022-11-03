c13f84d1b1be56ad53829653778c9d020c6c6853
Serenuy <dserena01@yahoo.com>
2022-11-02 20:46:40 -0500

    Update layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,12 @@
-import '../css/Home.css';
 import { useState } from 'react';
-import Select from 'react-select';
-import ColorThemes from '../data/ColorThemes';
-import Moods from '../data/Moods';
+import { useNavigate } from 'react-router-dom';
+import Settings from './Settings';
+import Music from './Music';
 
 const Home = () => {
-	const [ mood, setMood ] = useState("");
-
-	const changeTheme = (c) => {
-		localStorage.setItem("background", JSON.stringify(c));
-		document.body.style.backgroundColor = JSON.parse(localStorage.getItem("background"));
-	};
+	const [ mood, setMood ] = useState('');
+	const [ name, setName ] = useState(localStorage.getItem("username") ? JSON.parse(localStorage.getItem("username")) : JSON.parse(sessionStorage.getItem("username")));
+	const navigate = useNavigate();
 
 	const customStyles = {
 		control: (base,state) => ({
@@ -44,53 +40,46 @@ const Home = () => {
 		    primary25: "#ffb4c2",
 		    primary50: "#ffb4c2",
 		},
-	})
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		navigate("/planning-your-day/get-quote", {
+			state: {
+				mood: mood,
+			}
+		});
+	};
 
 	return(
 		<section id="homeSection">
-			<div className="home-grid">
-				<div className="home-item1">
-					<p>Change color theme:</p>
-					{
-						// Loop through ColorThemes JSON and return color picker buttons
-						ColorThemes.background.map(c => {
-							if(c.color) {
-								return(
-									<button key={c.color} className="btn btn-light me-2 mb-3 p-3 rounded-5" onClick={() => changeTheme(c.color)} style={{backgroundColor: c.color}}></button>
-								);
-							} else {
-								return(
-									<><br/><button key={c.default} className="btn btn-light rounded-5" onClick={() => changeTheme(c.default)}>Default</button></>
-								);
-							}
-						})
-					}
+			<Settings/>
+			<Music/>
+			<div className="container pos-absolute center-of-screen fadeInAnimation" id="homeContainer">
+				<div className="text-center">
+					<h2>Hi <em>{name}</em>, What's your goal for the day? To be.. </h2>
+					<button 
+					type="button" 
+					className="btn btn-light btn-lg rounded-5 me-2"
+					disabled={mood === "Motivated"}
+					onClick={e => setMood(e.target.innerText)}>Motivated</button>
+					<button 
+					type="button" 
+					className="btn btn-light btn-lg rounded-5"
+					disabled={mood === "Inspired"}
+					onClick={e => setMood(e.target.innerText)}>Inspired</button>
 				</div>
-				<div className="home-item2">
-					<div>
-						<label htmlFor="mood">How are you feeling right now:</label>
-						<Select
-						className="mt-2"
-						id="mood-input"
-						placeholder="Ex: sad, bored"
-						styles={customStyles}
-						theme={customThemes}
-						options={Moods}>
-						</Select>
-					</div>
-
-					<div className="mt-2">
-						<p>Suggestions:</p>
-						<button className="btn btn-transparent rounded-5 me-1">
-							Down
-						</button>
-						<button className="btn btn-transparent rounded-5 me-1">
-							Bored
-						</button>
-						<button className="btn btn-transparent rounded-5 me-1">
-							Lazy
-						</button>
-					</div>
+				<div className="text-center text-color mt-3">
+					<p>Goal of the day: <em>{mood}</em></p>
+				</div>
+				<div className="mt-3">
+					<button 
+					type="submit" 
+					className="btn btn-lg btn-dark btn-theme-1 center-block w-50 rounded-5 btn-next"
+					disabled={!mood}
+					onClick={handleSubmit}>
+					{mood ? 'Get '+mood: 'Waiting for selection..'}
+					</button>
 				</div>
 			</div>
 		</section>
