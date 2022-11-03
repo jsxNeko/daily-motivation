76e69e50f4940e53f25eeb3ba0cd9c45b7519ff4
Serenuy <dserena01@yahoo.com>
2022-11-02 20:51:13 -0500

    Create quote screen and implement API's

---
+++ src/components/Quote.js
@@ -0,0 +1,55 @@
+import Settings from './Settings';
+import Music from './Music';
+import axios from 'axios';
+import Loader from './Loader';
+import { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const Quote = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const type = location.state.mood;
+	const [ quote, setQuote ] = useState(''); 
+	const [ author, setAuthor ] = useState(''); 
+	const [ loading, setLoading ] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				if(type === "Inspirational") {
+					let response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=Inspirational`,
+						{headers: {"X-Api-Key": process.env.REACT_APIKEY},
+					});
+					setQuote(response.data[0].quote);
+					setAuthor(response.data[0].author);
+				} else {
+					let response = await axios.get(`https://goquotes-api.herokuapp.com/api/v1/random/1?type=tag&val=motivational`);
+					setQuote(response.data.quotes[0].text);
+					setAuthor(response.data.quotes[0].author);
+				}
+				setLoading(false);
+			} catch(error) {
+				alert(error);
+			}
+		}
+		fetchData();
+	},[]);
+
+	return(
+		<section id="homeSection">
+			<button className="btn btn-light bi-arrow-left pos-absolute" onClick={() => navigate("/planning-your-day")}/>
+			<Settings/>
+			<Music/>
+			{loading ? 
+				<Loader/> 
+				: 
+				<div className="center-of-screen pos-fixed text-center container fadeInAnimation" id="QuoteContainer">
+					<h1><i className="bi-quote"/>{quote}</h1>
+					<p>- {author}</p>
+				</div>
+			}
+		</section>
+	);
+};
+
+export default Quote;
