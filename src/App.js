9f31868f50d698c9b5b04e5fb38735dd0e160b4e
Serenuy <dserena01@yahoo.com>
2022-11-02 20:44:50 -0500

    Implemented custom cursor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import './App.css';
+import CustomCursor from 'custom-cursor-react';
 import Introduction from './components/Introduction';
-import UserInformation from './components/UserInformation';
+import EnterUserInformation from './components/EnterUserInformation';
+import ParticlesBox from './components/Particles';
 import Home from './components/Home';
-import { useEffect } from 'react';
+import Quote from './components/Quote';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Upon window loading, check if the user has selected a color previously
-window.onload = () => {
-  if(localStorage.getItem("background")) { 
-    document.body.style.backgroundColor = JSON.parse(localStorage.getItem("background"))
-  }
-};
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <BrowserRouter>
+      <ParticlesBox/>
+      <CustomCursor
+      className="pos-absolute"
+      targets={['.App']}
+      customClass="custom-cursor"
+      dimensions={58}
+      fill="TRANSPARENT"
+      strokeColor="#FFF"
+      opacity={1}
+      smoothness={{
+        movement: 0.2,
+        scale: 0.1,
+        opacity: 0.5,
+      }}
+      strokeWidth={5}
+      targetOpacity={0.9}
+      targetScale={1}
+      />
       <div className="App">
+      <ToastContainer position="top-center" limit={1} theme="dark"/>
         <Routes>
           <Route index element={<Introduction/>}/>
-          <Route path="/get-started" element={<UserInformation/>}/>
-          <Route path="/profile" element={<Home/>}/>
+          <Route path="/get-started" element={<EnterUserInformation/>}/>
+          <Route path="planning-your-day">
+            <Route index element={<Home/>}/>
+            <Route path="get-quote" element={<Quote/>}/>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
