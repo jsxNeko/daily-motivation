16825bdbee6b0446cd3be507743861a362ea747c
Serena Dean <dserena01@yahoo.com>
2022-12-29 02:18:43 -0600

    railway test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
 const cors = require('cors')
-app.use(cors({ origin: true }))
 dotenv.config()
 
 const app = express()
@@ -11,6 +10,8 @@ const port = process.env.PORT || 3000
 const buildPath = path.join(__dirname, 'build')
 
 app.use(express.static(buildPath))
+
+app.use(cors({ origin: true }))
 app.get('*', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'))
 })
