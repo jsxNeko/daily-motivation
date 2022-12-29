e09135bf4a07f8b6b7172fe544ffe27b7424bdd3
Serena Dean <dserena01@yahoo.com>
2022-12-29 01:56:21 -0600

    railway test

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
-// const buildPath = path.join(__dirname, 'build')
+const buildPath = path.join(__dirname, 'build')
 
-// app.use(express.static(buildPath))
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(buildPath, 'index.html'))
-// })
+app.use(express.static(buildPath))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`)
